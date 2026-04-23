bc82c1ebbc51151aa8b4febb788c132d02eac14e
Raymond <154872011+raymoondd@users.noreply.github.com>
2026-04-23 19:05:05 +0700

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -738,3 +738,11 @@ function syncTimerToSettings() {
 function contactme() {
     window.open("https://forms.gle/GMX5npeeqc2Qdu6SA", "_blank", "noopener,noreferrer");
 }
+
+.contact {
+    padding: 10px;
+    border-radius: 10px;
+    background-color: var(--primary-hover);
+    color: #fff;
+    font-size: 20px;
+}
